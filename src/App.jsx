28643a6224049404a2bfdfe72d51c354214fe90f
Antriksh5 <antrikshnahar@gmail.com>
2025-07-26 18:59:41 +0530

    add company section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/header/header.jsx';
 import React from 'react';
 import './App.css';
+import ComapanySection from './components/CompanySection/company.jsx';
 
 function App() {
   return (
     <>
       <Header />
+      <ComapanySection />
     </>
   );
 }
---
+++ src/components/CompanySection/company.jsx
@@ -0,0 +1,38 @@
+import airbnbLogo from '../../assets/airbnb.svg';
+import amazonLogo from '../../assets/amazon.svg';
+import facebookLogo from '../../assets/facebook.svg';
+import googleLogo from '../../assets/google.svg';
+import netflixLogo from '../../assets/netflix.svg';       
+import grabLogo from '../../assets/grab.svg';
+import './company.css';
+
+function ComapanySection(){
+
+    const companies = [
+        {name: 'Google', logo: googleLogo},
+        {name: 'Netflix', logo: netflixLogo},
+        {name: 'Airbnb', logo: airbnbLogo},
+        {name: 'Amazon', logo: amazonLogo},
+        {name: 'Facebook', logo: facebookLogo},
+        {name: 'Grab', logo: grabLogo},
+
+    ];
+    return (
+        <section className="company-section">
+            <h2 className='company-heading'>Trusted by 5000+ Companies Worldwide</h2>
+            <div className="company-logos">
+                {
+                    companies.map((company, index) => (
+                        <img 
+                         key={index} 
+                         src={company.logo} 
+                         alt= {company.name} 
+                         className='company-logo'
+                         />
+                    ))
+                }
+            </div>
+        </section>
+    );
+}
+export default ComapanySection;
