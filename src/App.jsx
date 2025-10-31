500e576edb80667b2a5f004e86096798750b0a0c
Antriksh5 <antrikshnahar@gmail.com>
2025-11-01 00:57:35 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from './components/header/header.jsx';
 import React from 'react';
 import './App.css';
+import Header from './components/header/Header.jsx';
 import CompanySection from './components/CompanySection/Company.jsx';
 import AllInOne from './components/AllInOne/AllInOne.jsx';
 import WhatIsSkilline from './components/WhatIsSkilline/Wis.jsx';
